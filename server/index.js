6801f28e5ac3dc5d33d43394c2696b3ac0c4539a
floor777 <bvfwra@gmail.com>
2022-12-22 14:31:56 -0800

    Initial server app commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({message: 'Hello World!'})
+})
+
+app.get('/api', (req, res) => {
+    res.json({ message: "how lovely"});
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
+
